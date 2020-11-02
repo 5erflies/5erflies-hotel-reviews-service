4b748cff613b66cf344e7e10a08023163d634520
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-11-02 13:08:11 -0800

    Change alert message

--- client/src/components/ReviewItem.jsx
+++ client/src/components/ReviewItem.jsx
@@ -41,7 +41,7 @@ class ReviewItem extends React.Component {
   }
 
   goToUserPage() {
-    alert("This button doesn't actually do anything because I didn't make a user page because that wasn't part of my role!");
+    alert("This button is not functional yet, and will be implemented in a future update");
   }
 
   render() {
